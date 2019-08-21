906636d36fb04e36bf5c2923b9905842bf0f09f3
wmgreenlee <wmgreenlee@PLD-LAB-400-9T.fayette.ketsds.net>
2019-08-21 11:49:52 -0400

    Changed Main

--- FightingGame/src/TheWorks/Main.java
+++ FightingGame/src/TheWorks/Main.java
@@ -8,13 +8,18 @@ public class Main {
 
 		Start start = new Start();
 
-		if (mode == 2) {
+		if (mode == 1) {
+			// initiate 1 player mode
+		} else {
+			if (mode == 2) {
 
-			Playspace PlayArea = new Playspace();
-			Player P1 = new Player1();
-			Player P2 = new Player2();
+				// initiate 2 player mode
+				Playspace PlayArea = new Playspace();
+				Player P1 = new Player1();
+				Player P2 = new Player2();
 
-			PlayArea.add(P1, P2);
+				PlayArea.add(P1, P2);
+			}
 		}
 	}
 
