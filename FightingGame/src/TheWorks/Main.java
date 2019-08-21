566a5478372130c18ece2bbdef0e4ae21b75308c
wmgreenlee <wmgreenlee@PLD-LAB-400-9T.fayette.ketsds.net>
2019-08-21 12:38:28 -0400

    or this one

--- FightingGame/src/TheWorks/Main.java
+++ FightingGame/src/TheWorks/Main.java
@@ -3,30 +3,37 @@ package TheWorks;
 public class Main {
 
 	static int mode;
+	static boolean pressed;
 
 	public static void main(String[] args) {
 
 		mode = 0;
+		pressed = false;
 
 		Start start = new Start();
 
-		if (mode == 1) {
-			// initiate 1 player mode
-		} else {
-			if (mode == 2) {
+		while (pressed)
+			if (mode == 1) {
+				// initiate 1 player mode
+			} else {
+				if (mode == 2) {
 
-				// initiate 2 player mode
-				Playspace PlayArea = new Playspace();
-				Player P1 = new Player1();
-				Player P2 = new Player2();
+					// initiate 2 player mode
+					Playspace PlayArea = new Playspace();
+					Player P1 = new Player1();
+					Player P2 = new Player2();
 
-				PlayArea.add(P1, P2);
+					PlayArea.add(P1, P2);
+				}
 			}
-		}
 	}
 
 	public static void setMode(int m) {
 		mode = m;
+	}
+
+	public static void pressed() {
+		pressed = true;
 	}
 
 }
