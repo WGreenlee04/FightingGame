8a10babfa80e41a70eab1a79528a142b2f25c154
wmgreenlee <wmgreenlee@PLD-LAB-400-9T.fayette.ketsds.net>
2019-08-26 11:58:43 -0400

    pavan angre again

--- FightingGame/src/TheWorks/Main.java
+++ FightingGame/src/TheWorks/Main.java
@@ -15,16 +15,14 @@ public class Main {
 		while (pressed) {
 			if (mode == 1) {
 				// initiate 1 player mode
-			} else {
-				if (mode == 2) {
+			} else if (mode == 2) {
 
-					// initiate 2 player mode
-					Playspace PlayArea = new Playspace();
-					Player P1 = new Player1();
-					Player P2 = new Player2();
+				// initiate 2 player mode
+				Playspace PlayArea = new Playspace();
+				Player P1 = new Player1();
+				Player P2 = new Player2();
 
-					PlayArea.add(P1, P2);
-				}
+				PlayArea.add(P1, P2);
 			}
 		}
 	}
