372ca8ad1fef4c0fb75513319a6832d0b6241177
WMGreenlee <greenlee04@gmail.com>
2019-09-23 19:00:40 -0400

    added placeholder item, and found bug

---
+++ FightingGame/src/TheWorks/Fist.java
@@ -0,0 +1,15 @@
+package TheWorks;
+
+import javax.swing.ImageIcon;
+
+public class Fist extends Item {
+
+	public Fist(Playspace space) {
+		super(20, 30, 0, 0, 20, 100, new ImageIcon("src/resources/weaponFist.png").getImage(), 80, 80, space);
+	}
+
+	@Override
+	public String getName() {
+		return "weaponStick";
+	}
+}
--- FightingGame/src/TheWorks/Player.java
+++ FightingGame/src/TheWorks/Player.java
@@ -43,6 +43,11 @@ public class Player {
 		image = Tools.scalePlayer(image, this);
 		this.imageDir = imageDir;
 		this.darkImageDir = darkImageDir;
+
+		if (item == null) {
+			item = new Fist(space);
+			item.setPlayer(this);
+		}
 	}
 
 	public Point leftHandItemLocation() {
