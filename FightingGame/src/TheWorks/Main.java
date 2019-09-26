700b18e4303713eadba5c2ec458669614a0923d6
WMGreenlee <greenlee04@gmail.com>
2019-09-26 17:46:00 -0400

    REWORKING ALL CODE, MIGHT USE LIBRARY LWJGL

--- FightingGame/src/TheWorks/Main.java
+++ FightingGame/src/TheWorks/Main.java
@@ -2,37 +2,12 @@ package TheWorks;
 
 public class Main {
 
-	private static int mode;
-	private static Runnable r;
-
 	public static void main(String[] args) {
-
-		mode = 0;
-		Start startWindow = new Start();
-		startWindow.pingUser();
-
-		r = new Runnable() {
-			public void run() {
-				if (mode == 1) {
-					// initiate 1 player mode
-					Application application = new Application();
-					application.initiateFrame(mode);
-				} else if (mode == 2) {
-
-					// initiate 2 player mode
-					Application application = new Application();
-					application.initiateFrame(mode);
-				}
-			}
-		};
+		Main mainGame = new Main();
 	}
 
-	public static void setMode(int m) {
-		mode = m;
-	}
-
-	public static Runnable getRun() {
-		return r;
+	public Main() {
+		// from here we need to call the application, which calls the playspace
 	}
 
 }
